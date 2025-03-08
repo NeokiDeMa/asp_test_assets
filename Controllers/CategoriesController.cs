e1a45eb2f1cf8fe09e7a5f42c736d7b9abc90a57
sajjam <sajjad2729115@gmail.com>
2025-03-08 17:19:10 +0330

    update

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using OfficeOpenXml;
-using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AssetServer.Controllers
 {
@@ -9,33 +6,35 @@ namespace AssetServer.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        //private readonly string IconsPath = "https://net8-web-api-test.up.railway.app/file/iconss/";
-        //private readonly string AssetsPath = "https://net8-web-api-test.up.railway.app/file/assetss/";
+        private readonly string IconsPath = "https://avatars-backend-production.up.railway.app/static/iconss/";
+        private readonly string AssetsPath = "https://avatars-backend-production.up.railway.app/static/assetss/";
 
-        private readonly string IconsPath = "http://localhost/iconss/";
-        private readonly string AssetsPath = "http://localhost/assetss/";
+        //private readonly string IconsPath = "http://localhost/iconss/";
+        //private readonly string AssetsPath = "http://localhost/assetss/";
 
         private readonly string _filePath = Path.Combine(Directory.GetCurrentDirectory(), "Book1.xlsx");
+
+        private Database database = new Database();
 
         [HttpGet("find-all")]
         public IActionResult GetAssetsByCategoryId([FromQuery] string categoryId)
         {
-            var categories = ReadCategoriesFromExcel();
-            var assets = ReadAssetsFromExcel();
-
-            // پیدا کردن دسته‌بندی با همان categoryId
-            var category = categories.FirstOrDefault(c => c.Id == categoryId);
+            var category = database.categories.FirstOrDefault(c => c.id == categoryId);
 
             if (category == null)
-            {
                 return NotFound("Category not found.");
-            }
 
-            // فیلتر کردن assets مربوط به categoryId
-            var categoryAssets = assets.Where(a => a.CategoryId == categoryId).ToList();
+            category.icon = IconsPath + category.icon;
 
-            // اضافه کردن assets به دسته‌بندی
+            var categoryAssets = database.assets.Where(a => a.categoryId == categoryId).ToList();
+
             category.Assets = categoryAssets;
+
+            category.Assets.ForEach(c =>
+            {
+                c.icon = IconsPath + c.icon;
+                if (!string.IsNullOrEmpty(c.file)) c.file = AssetsPath + c.file;
+            });
 
             return Ok(new[] { category });
         }
@@ -43,125 +42,34 @@ namespace AssetServer.Controllers
         [HttpGet("parents")]
         public IActionResult GetParentCategories()
         {
-            var categories = ReadCategoriesFromExcel();
-            var parentCategories = categories.Where(c => c.ParentCategoryId == null).ToList();
+            var parentCategories = database.categories.Where(c => string.IsNullOrEmpty(c.parentCategoryId)).ToList();
+            parentCategories.ForEach(c =>
+            {
+                c.icon = IconsPath + c.icon;
+            });
             return Ok(parentCategories);
         }
 
         [HttpGet("find-category")]
         public IActionResult GetSubCategories([FromQuery] string categoryId)
         {
-            var categories = ReadCategoriesFromExcel();
-
-            // پیدا کردن دسته‌بندی که parentCategoryId برابر با categoryId است
-            var parentCategory = categories.FirstOrDefault(c => c.Id == categoryId);
-
+            var parentCategory = database.categories.FirstOrDefault(c => c.id == categoryId);
+            parentCategory.icon = IconsPath + parentCategory.icon;
             if (parentCategory == null)
-            {
                 return NotFound("Category not found.");
-            }
-
-            // پیدا کردن تمامی زیرمجموعه‌های این دسته‌بندی
-            var subCategories = categories
-                .Where(c => c.ParentCategoryId == categoryId)
+            
+            var subCategories = database.categories
+                .Where(c => c.parentCategoryId == categoryId)
                 .ToList();
 
-            // اضافه کردن زیرمجموعه‌ها به parentCategory
+            subCategories.ForEach(c =>
+            {
+                c.icon = IconsPath + c.icon;
+            });
+
             parentCategory.SubCategory = subCategories;
 
-            // بازگشت پاسخ با parentCategory و زیرمجموعه‌ها
             return Ok(new[] { parentCategory });
         }
-
-        private List<Category> ReadCategoriesFromExcel()
-        {
-            var categories = new List<Category>();
-
-            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-            using (var package = new ExcelPackage(new FileInfo(_filePath)))
-            {
-                var worksheet = package.Workbook.Worksheets[0]; // فرض می‌کنیم داده‌ها در شیت اول هستند
-                int rowCount = worksheet.Dimension.Rows;
-
-                for (int row = 2; row <= rowCount; row++) // فرض بر این که سطر اول هدر است
-                {
-                    categories.Add(new Category
-                    {
-                        Id = worksheet.Cells[row, 1].Text,
-                        Icon = IconsPath + worksheet.Cells[row, 2].Text,
-                        Gender = worksheet.Cells[row, 3].Text,
-                        Name = worksheet.Cells[row, 4].Text,
-                        Username = worksheet.Cells[row, 5].Text,
-                        ParentCategoryId = string.IsNullOrEmpty(worksheet.Cells[row, 6].Text) ? null : worksheet.Cells[row, 6].Text
-                    });
-                }
-            }
-            return categories;
-        }
-
-        private List<Asset> ReadAssetsFromExcel()
-        {
-            var assets = new List<Asset>();
-
-            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-            using (var package = new ExcelPackage(new FileInfo(_filePath)))
-            {
-                var worksheet = package.Workbook.Worksheets[1]; // فرض می‌کنیم داده‌ها در شیت دوم هستند
-                int rowCount = worksheet.Dimension.Rows;
-
-                for (int row = 2; row <= rowCount; row++) // فرض بر این که سطر اول هدر است
-                {
-                    string file = "";
-                    if (!string.IsNullOrEmpty( worksheet.Cells[row, 2].Text))
-                    {
-                        var stArr = worksheet.Cells[row, 2].Text.Split(',');
-                        for (int i = 0; i < stArr.Count(); i++)
-                            stArr[i] = AssetsPath + stArr[i];
-                        file = string.Join(',',stArr);
-                    }
-
-                    assets.Add(new Asset
-                    {
-                        Id = new Guid().ToString(),
-                        Icon = IconsPath + worksheet.Cells[row, 1].Text,
-                        File = file,
-                        Gender = worksheet.Cells[row, 3].Text,
-                        Name = worksheet.Cells[row, 4].Text,
-                        Username = worksheet.Cells[row, 5].Text,
-                        Type = worksheet.Cells[row, 6].Text,
-                        Data = worksheet.Cells[row, 7].Text,
-                        CategoryId = worksheet.Cells[row, 8].Text,
-                    });
-                }
-            }
-            return assets;
-        }
-    }
-
-
-
-    public class Asset
-    {
-        public string Id { get; set; }
-        public string Icon { get; set; }
-        public string File { get; set; }
-        public string Gender { get; set; }
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Type { get; set; }
-        public string Data { get; set; }
-        public string CategoryId { get; set; }
-    }
-
-    public class Category
-    {
-        public string Id { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public string Icon { get; set; }
-        public string Gender { get; set; }
-        public string ParentCategoryId { get; set; }
-        public List<Category> SubCategory { get; set; }
-        public List<Asset> Assets { get; set; }
     }
 }
