366d514e72dc0e23321089585d122a350703bc65
sajjam <sajjad2729115@gmail.com>
2025-04-07 18:26:42 +0330

    update the asp net server

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,8 +9,8 @@ namespace AssetServer.Controllers
         private readonly string IconsPath = "https://avatars-backend-production.up.railway.app/static/iconss/";
         private readonly string AssetsPath = "https://avatars-backend-production.up.railway.app/static/assetss/";
 
-        //private readonly string IconsPath = "http://localhost/iconss/";
-        //private readonly string AssetsPath = "http://localhost/assetss/";
+        //private readonly string IconsPath = "http://localhost:9000/iconss/";
+        //private readonly string AssetsPath = "http://localhost:9000/assetss/";
 
         private readonly string _filePath = Path.Combine(Directory.GetCurrentDirectory(), "Book1.xlsx");
 
